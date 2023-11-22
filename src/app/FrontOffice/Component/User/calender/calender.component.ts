833fe2feed0363ac974c1a7bd8d1fd8cfa619bb4
dhouha belakhel <dhouhabelakhel2001@gmail.com>
2023-11-22 21:04:48 +0100

    add calender to user profil

--- src/app/FrontOffice/Component/User/calender/calender.component.ts
+++ src/app/FrontOffice/Component/User/calender/calender.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
+import { DateAdapter } from '@angular/material/core';
+import { MatCalendarCellClassFunction, MatCalendarCellCssClasses } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-calender',
@@ -8,5 +9,16 @@ import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 })
 export class CalenderComponent {
 
+  specialDates =  ['11/130/2023', '11/11/2023']; // Dates spéciales
+
+  constructor(private dateAdapter: DateAdapter<Date>) {}
+
+  dateClass(date: Date): MatCalendarCellCssClasses {
+    const dateString = this.dateAdapter.format(date, 'yyyy-MM-dd');
+    console.log('Date actuelle : ', dateString);
+    const isSpecial = this.specialDates.includes(dateString);
+    console.log('Est une date spéciale : ', isSpecial);
+    return isSpecial ? 'special-date' : '';
+  }
 
 }
