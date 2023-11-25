2eaf99bb084862411c9c690069f15ce3539c3aa4
dhouha belakhel <dhouhabelakhel2001@gmail.com>
2023-11-25 11:01:17 +0100

    blog's form validators

--- src/app/FrontOffice/Component/blogs/add-form/add-form.component.ts
+++ src/app/FrontOffice/Component/blogs/add-form/add-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Blog } from 'src/app/classes/blog';
 import { User } from 'src/app/classes/user';
 import { BlogsService } from 'src/app/services/blogs.service';
@@ -12,22 +12,46 @@ import { BlogsService } from 'src/app/services/blogs.service';
 export class AddFormComponent implements OnInit{
   addblog!:FormGroup;
  user!:User;
+ userData!:string|null;
   @Output() newBlog= new EventEmitter<Blog>();
  constructor(private blogsService: BlogsService,private formBuilder:FormBuilder) {}
   ngOnInit(): void {
-    const userData=localStorage.getItem("session");
-    if(userData){
-    this.user=JSON.parse(userData);
-    this.addblog=this.formBuilder.nonNullable.group({
-      title:[''],
+   this.userData=localStorage.getItem("session");
+    if(this.userData){
+    this.user=JSON.parse(this.userData);
+    this.addblog=this.formBuilder.group({
+      title:['',[Validators.minLength(6),Validators.required,Validators.pattern('^[A-Z].*')]],
       author:[this.user.userName],
-      content:[''],
+      content:['',[Validators.required,Validators.maxLength(10)]],
    })
   }}
+  public get title() {
+    return this.addblog.get('title');
+  }
+  public get content(){
+return this.addblog.get('content');
+  }
   saveblog(){
  this.blogsService.saveblog(this.addblog.value).subscribe(data=>{this.newBlog.emit(data);});
   }
+ isValidContent(){
+  return this.content?.errors?.['required']  && this.content?.dirty;
+ }
+ isValidContentLength(){
+  return this.content?.errors?.['maxlength']  && this.content?.dirty;
+ }
+  isValidTitlePattren(): boolean {
+    return this.title?.errors?.['pattern'] && this.title?.dirty;
+  }
+  isValidTitleLength(): boolean {
+    return this.title?.errors?.['minlength'] && this.title?.touched;
+  }
+ 
+  isValidTitle(): boolean {
+    return this.title?.errors?.['required'] && this.title?.dirty;
+  }
 
+  
 
 
 
